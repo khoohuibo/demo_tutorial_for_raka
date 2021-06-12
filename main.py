f592343c27720d0881f31fcd07c110153d1f19e0
StarFighter156 <starfighter156@gmail.com>
2021-06-13 00:33:05 +0800

    I am boy
    Signed-off-by: StarFighter156 <starfighter156@gmail.com>

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ Created on Sat Jun 12 20:56:06 2021
 
 import Source.ergy
 import Source.time
-
-smdaskdkmasdmksadas = 0
-sdkamsdlas = 0
-mother = 0
+#1. Communism Jokes are not funny until everyone gets it 
+#2.In the economic sphere, communism calls for the government to take control of all the capital and industry in the country in an effort to get rid of economic inequality. On the other hand, a democracy respects individuals' right to own property and means of production.
+#3.The political landscape is also very different in a democracy vs. under communism. In a democratic society people are free to create their own political parties and contest in elections, which are free of coercion and fair to all contestants. In a communist society, however, the government is controlled by one political party and political dissent is not tolerated.
