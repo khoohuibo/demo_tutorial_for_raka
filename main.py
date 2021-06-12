7416ad8839f0a6f75dcf758b4d87abaa0c243126
chiefengineerofficer <vaikesavan@gmail.com>
2021-06-13 00:33:03 +0800

    damm ngfsouevfpgif[j]pkawfbgowiewrn[ i am raman

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ Created on Sat Jun 12 20:56:06 2021
 import Source.ergy
 import Source.time
 
-smdaskdkmasdmksadas = 0
-sdkamsdlas = 0
-mother = 0
+
+#1.Democracy is not spelled as Communism
+#2.There is a Y in democracy while there is a m in a communism
+#3.Democracy gives a freemdom while communism doesn't
+#addtitional singapore is supposedly democratic
