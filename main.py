7ab24df53e2935f8d4f31aeecf088aa8ab26eedb
Hubert Khoo <hubert.khoo@sydereal.com>
2021-06-13 00:32:48 +0800

    sdmaksdas

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ Created on Sat Jun 12 20:56:06 2021
 import Source.ergy
 import Source.time
 
-smdaskdkmasdmksadas = 0
-sdkamsdlas = 0
-mother = 0
+# Write the answer to this question in the section below
+# What's the three main differences between democracy and communism?
+# Answer: 
+# 1.
+# 2.
+# 3.
+0
